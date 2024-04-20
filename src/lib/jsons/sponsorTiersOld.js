1b21952c715abda9666c20c11215eec18db1b45d
tyranasaurus <90056513+tyranasaurus@users.noreply.github.com>
2024-04-20 05:52:13 -0700

    sponsor

---
+++ src/lib/jsons/sponsorTiersOld.js
@@ -0,0 +1,101 @@
+const sponsorTiers = [
+	{
+		name: "Unicorn Sponsors",
+		singular: "Unicorn Sponsor",
+		headerColor: "#F9C4CA",
+		sponsors: [
+			{
+				url: "sponsors/stallion/Wolfram.png",
+				alt: "Wolfram Alpha",
+				link: "https://www.wolfram.com/",
+			},
+		],
+	},
+	{
+		name: "Stallion Sponsors",
+		singular: "Stallion Sponsor",
+		headerColor: "#5ba89a",
+		sponsors: [
+			{
+				url: "sponsors/foal/AlphaStar.png",
+				alt: "Alphastar Academy",
+				link: "https://alphastar.academy/",
+				blurb:
+					"AlphaStar Academy (alphastar.academy) offers extensive training programs for gifted and high achieving students towards national and international Math and Science competitions such as MathCounts, American Mathematics Competitions, USA Math Olympiads, USA Computing Olympiads, and USA Physics Olympiads.",
+			},
+		],
+	},
+	{
+		name: "Colt Sponsors",
+		singular: "Colt Sponsor",
+		headerColor: "#4d9464",
+		sponsors: [
+			{
+				url: "sponsors/colt/AwesomeMath.png",
+				alt: "AwesomeMath",
+				link: "https://www.awesomemath.org",
+			},
+			{
+				url: "sponsors/colt/Brilliant.png",
+				alt: "Brilliant",
+				link: "https://www.brilliant.org",
+			},
+		],
+	},
+	{
+		name: "Foal Sponsors",
+		singular: "Foal Sponsor",
+		headerColor: "#ad8c9e",
+		sponsors: [
+			{
+				url: "sponsors/foal/BBMC Math.png",
+				alt: "BBMC Math",
+				link: "https://bbmc-math.com/",
+			},
+			{
+				url: "sponsors/foal/AoPS.png",
+				alt: "Art of Problem Solving",
+				link: "https://aops.com",
+			},
+			{
+				url: "sponsors/foal/Texas Instruments.png",
+				alt: "Texas Instruments",
+				link: "https://www.ti.com/",
+			},
+		],
+	},
+	{
+		name: "Partners",
+		singular: "Partner",
+		headerColor: "#1B9AAA",
+		sponsors: [
+			{
+				url: "sponsors/partners/Atomic Grader.png",
+				alt: "Atomic Grader",
+				link: "https://atomicgrader.com/",
+			},
+			{
+				url: "sponsors/partners/Berkeley Math Tournament.png",
+				alt: "Berkeley Math Tournament",
+				link: "https://bmt.berkeley.edu/",
+			},
+			{
+				url: "sponsors/partners/ContestDojo.png",
+				alt: "ContestDojo",
+				link: "https://contestdojo.com/",
+			},
+			{
+				url: "sponsors/partners/Lexington Math Tournament.svg",
+				alt: "Lexington Math Tournament",
+				link: "https://lhsmath.org/LMT/Home",
+			},
+			{
+				url: "sponsors/partners/SIMCC.png",
+				alt: "SIMCC",
+				link: "https://simcc.org/",
+			},
+		],
+	},
+];
+
+export default sponsorTiers;
