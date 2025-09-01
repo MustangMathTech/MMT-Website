d55962eee9afb24adcf6039d9b748d9b3d0aac1c
Abhiram KKuchi <abhiramk@mustangmath.com>
2025-08-31 21:41:13 -0700

    changed instructor page to have transition and end up in the middle

--- src/routes/classes/instructors/+page.svelte
+++ src/routes/classes/instructors/+page.svelte
@@ -3,8 +3,10 @@
     import FlexBox from '$lib/components/FlexBox.svelte';
     import PageHeader from '$lib/components/PageHeader.svelte';
     import { onMount } from "svelte";
+    import "svelte/easing";
+    import "svelte/animate";
 
-    let windowWidth;
+let windowWidth;
 </script>
 <svelte:window bind:innerWidth={windowWidth}/>
 
@@ -17,15 +19,63 @@
 <br><br>
 
 <FlexBox wrap={true}>
-    <InfoPerson width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Photo - Alexander Liu.jpg" name="Alexander Liu" description="Alexander Liu is a high school junior in Massachusetts. He has participated in math competitions since elementary school, including getting 24th place in MA MATHCOUNTS state, and achieving DHR in the 2024 AMC10A and B. Outside of math, he enjoys reading, playing piano, playing badminton, playing video games, and playing ultimate." left/>
+<div id = "first"><InfoPerson width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Photo - Alexander Liu.jpg" name="Alexander Liu" description="Alexander Liu is a high school junior in Massachusetts. He has participated in math competitions since elementary school, including getting 24th place in MA MATHCOUNTS state, and achieving DHR in the 2024 AMC10A and B. Outside of math, he enjoys reading, playing piano, playing badminton, playing video games, and playing ultimate." left/></div>
 
-    <InfoPerson width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Margaret_Sikes_Serious - Margaret Sikes.png" name="Margaret Sikes" description="Margaret is a homeschooled high school junior in California. She began her competition math journey in middle school, and her favorite competition is USAMTS because of the unique competition structure and opportunity to research challenging problems. She has close to two years of experience in tutoring, and is a member of the Curriculum Development and Problem Writing Teams at Mustang Math. Margaret's hobbies include doing math, reading, writing, chess, and much more." right/>
+    <div id = "second"><InfoPerson  width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Margaret_Sikes_Serious - Margaret Sikes.png" name="Margaret Sikes" description="Margaret is a homeschooled high school junior in California. She began her competition math journey in middle school, and her favorite competition is USAMTS because of the unique competition structure and opportunity to research challenging problems. She has close to two years of experience in tutoring, and is a member of the Curriculum Development and Problem Writing Teams at Mustang Math. Margaret's hobbies include doing math, reading, writing, chess, and much more." right/></div>
 
-    <InfoPerson width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Anika_Gupta_Serious - Anika Gupta.png" name="Anika Gupta" description="Anika is a high school junior in Washington and has participated in math competitions since third grade, including Math Kangaroo and AMC. Anika is part of the Curriculum Development team for Mustang Math. She is passionate about teaching others and has experience tutoring students online. Outside of math, Anika enjoys reading, baking, and traveling." left/>
+    <div id = "third"><InfoPerson  width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Anika_Gupta_Serious - Anika Gupta.png" name="Anika Gupta" description="Anika is a high school junior in Washington and has participated in math competitions since third grade, including Math Kangaroo and AMC. Anika is part of the Curriculum Development team for Mustang Math. She is passionate about teaching others and has experience tutoring students online. Outside of math, Anika enjoys reading, baking, and traveling." left/></div>
 
-    <InfoPerson width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/IMG_7776 - Girish Thillai Natarajan.jpeg" name="Girish Thillai Natarajan" description="Girish Thillai Natarajan is a high school junior in California. He has participated in math contests such as BMT, SMT, and the AMC series beginning in middle school. He has experience tutoring students in math both in person and online at many levels of difficulty. At Mustang Math, Girish is a member of the Curriculum Development team. Outside of math, he enjoys reading, programming, and playing video games." right/>
+    <div id= "fourth"><InfoPerson  width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/IMG_7776 - Girish Thillai Natarajan.jpeg" name="Girish Thillai Natarajan" description="Girish Thillai Natarajan is a high school junior in California. He has participated in math contests such as BMT, SMT, and the AMC series beginning in middle school. He has experience tutoring students in math both in person and online at many levels of difficulty. At Mustang Math, Girish is a member of the Curriculum Development team. Outside of math, he enjoys reading, programming, and playing video games." right/></div>
 
-    <InfoPerson width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Daniel_Swetman_Serious - Daniel Swetman.png" name="Daniel Swetman" description="Daniel is a high school junior in Washington. He has participated in various math competitions over the years such as MATHCOUNTS, AMC 8, and AMC 10. He is a part of the Problem Writing and Curriculum Development Team for Mustang Math, but also helps with teaching students in his school's math team. Apart from math, he enjoys drawing and playing video games." left/>
+    <div id = "fifth"><InfoPerson width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Daniel_Swetman_Serious - Daniel Swetman.png" name="Daniel Swetman" description="Daniel is a high school junior in Washington. He has participated in various math competitions over the years such as MATHCOUNTS, AMC 8, and AMC 10. He is a part of the Problem Writing and Curriculum Development Team for Mustang Math, but also helps with teaching students in his school's math team. Apart from math, he enjoys drawing and playing video games." left/></div>
 
-    <InfoPerson width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Sara_Liu_Instructor.png" name="Sara Liu" description="Sara is a high school sophomore in Washington. She has participated in competitions such as the AMC series and AIME. She has tutored younger students in middle school, and she is also one of the leaders of her school's math club. Outside of math, she enjoys music, badminton, and birdwatching." right/>
+    <div id = "sixth"><InfoPerson width={windowWidth < 700 ? "90%" : "50%"} pic="/about-us-images/serious/Sara_Liu_Instructor.png" name="Sara Liu" description="Sara is a high school sophomore in Washington. She has participated in competitions such as the AMC series and AIME. She has tutored younger students in middle school, and she is also one of the leaders of her school's math club. Outside of math, she enjoys music, badminton, and birdwatching." right/></div>
 </FlexBox>
+
+<style>
+
+    #first {
+        display: flex;
+        justify-content: center;
+        animation: moveRight 5s;
+    }
+  
+     #second {
+       display: flex;
+        justify-content: center;
+        animation: moveLeft 5s;
+    }
+    #third {
+        display: flex;
+        justify-content: center;
+        animation: moveRight 5s;
+    }  
+    #fourth {
+        display: flex;
+        justify-content: center;
+        animation: moveLeft 5s;
+    } 
+    #fifth {
+        display: flex;
+        justify-content: center;
+        animation: moveRight 5s;
+    } 
+     #sixth {
+        display: flex;
+        justify-content: center;
+        animation: moveLeft 5s;
+    } 
+    @keyframes moveRight{
+       from {
+        transform: translate(50em)
+       }
+    }
+    @keyframes moveLeft {
+        from {
+            transform: translate(-50em)
+        }
+    }
+   
+   
+   
+</style>
