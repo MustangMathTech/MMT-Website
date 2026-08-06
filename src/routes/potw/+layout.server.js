55d2086ea6571434e296e7df2105be97b0aab338
Geonwoo Kim <124229577+geonnwwoo@users.noreply.github.com>
2026-08-05 22:17:28 -0700

    made potw changes automatic based on time (weekly change)

--- src/routes/potw/+layout.server.js
+++ src/routes/potw/+layout.server.js
@@ -1,17 +1,25 @@
 import fs from 'fs';
 import path from 'path';
-
 export async function load() {
+
+    // NOTE: this is secure. It checks the server's date, etc to see which
+    // problem to display. User cannot manipulate their own device to access
+    // problem or answer.
+
+    const now = new Date();
+
     const filePath = path.resolve('src/routes/potw/server/potw.json');
     const potwData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-    
-    const latestPotw = potwData.at(-1);
-    const pastPotw = potwData.slice(0, -1);
 
+
+    // START TIME. MODIFY TO CHANGE POTW SELECTION instead of server/potw.json
+    const startDate = new Date('2026-08-05T08:00:00Z');
+    const weekIndex = Math.floor((now-startDate)/(1000*60*60*24*7));
+    const problemIndex = weekIndex % potwData.length;
+    const latestPotw = potwData[problemIndex];
     const { answer, solution, ...safeLatestPotw } = latestPotw;
 
     return {
-        potw: safeLatestPotw,
-        archive: pastPotw
+        potw: safeLatestPotw
     };
 }
--- src/routes/potw/archive/+page.server.js
+++ src/routes/potw/archive/+page.server.js
@@ -4,10 +4,15 @@ import path from 'path';
 export async function load() {
     const filePath = path.resolve('src/routes/potw/server/potw.json');
     const potwData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    const startDate = new Date('2024-01-01T00:00:00Z');
+    const now = new Date();
+    const weekIndex = Math.floor((now - startDate) / (1000 * 60 * 60 * 24 * 7));
+    const problemIndex = weekIndex % potwData.length;
+    const archive = potwData.filter((_, index) => index !== problemIndex).reverse();
 
-    // Return all but the most recent problem
-    const archive = potwData.slice(0, -1).reverse();
+
+
     return {
-        archive
+        archive: archive
     };
 }
