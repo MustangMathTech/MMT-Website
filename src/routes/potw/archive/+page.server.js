214b84c9edaaf9a2a64499142cc202f5ca917414
Geonwoo Kim <124229577+geonnwwoo@users.noreply.github.com>
2026-08-10 18:14:39 -0700

    fixed deployment error with potw/archive; problem with hardcoding paths

--- src/routes/potw/archive/+page.server.js
+++ src/routes/potw/archive/+page.server.js
@@ -1,9 +1,6 @@
-import fs from 'fs';
-import path from 'path';
+import potwData from '../server/potw.json';
 
 export async function load() {
-    const filePath = path.resolve('src/routes/potw/server/potw.json');
-    const potwData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
     
     // START TIME. MODIFY TO CHANGE POTW SELECTION instead of server/potw.json
     const startDate = new Date('2026-08-05T08:00:00Z');
